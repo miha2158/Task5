733d61d982398f4964e95039ca959d319d3803a9
miha2158 <miha.vanya@gmail.com>
2017-07-19 22:16:10 +0500

    Сделал #1

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -4,12 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using static System.Console;
+
 namespace Task5
 {
     class Program
     {
         static void Main(string[] args)
         {
+            WriteLine("Введите длину массива");
+
+            int n = 0;
+            while (!int.TryParse(ReadLine(), out n) || n <= 0)
+                WriteLine("Введите положительное целое число");
+
+            var arr = new int[n, n];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    if(i > j)
+                        if ((n - i + 1) > j)
+                            arr[i,j] = 1;
+                }
+
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    Write(" {0}",arr[i,j]);
+            }
+
+            ReadKey(true);
         }
     }
 }
